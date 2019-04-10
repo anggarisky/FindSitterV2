e78df9cd33f0183acbb6cab2824fc51c775703fe
anggarisky <anggariskysetiawan@gmail.com>
2019-04-10 19:19:01 +0700

    new remove firebase item

--- app/src/main/java/com/example/findsitter/MainActivity.java
+++ app/src/main/java/com/example/findsitter/MainActivity.java
@@ -3,8 +3,11 @@ package com.example.findsitter;
 import android.Manifest;
 import android.content.pm.PackageManager;
 import android.location.Location;
+import android.os.Handler;
 import android.support.annotation.NonNull;
 import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v4.content.PermissionChecker;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
@@ -13,28 +16,41 @@ import android.view.View;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import com.firebase.geofire.GeoFire;
-import com.firebase.geofire.GeoLocation;
 import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationCallback;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationResult;
 import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.location.LocationSettingsRequest;
+import com.google.android.gms.location.LocationSettingsResponse;
+import com.google.android.gms.location.SettingsClient;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
+import com.nabinbhandari.android.permissions.PermissionHandler;
+import com.nabinbhandari.android.permissions.Permissions;
 
 import java.util.ArrayList;
+
+import static android.Manifest.permission.ACCESS_FINE_LOCATION;
 
 public class MainActivity extends AppCompatActivity {
 
     RecyclerView sitter_place;
-    DatabaseReference reference, reference2;
+    DatabaseReference reference, reference2, reference3;
     ArrayList<Sitter> list;
     SitterAdapter sitterAdapter;
     FusedLocationProviderClient fusedLocationProviderClient;
+    private LocationRequest locationRequest;
+    private LocationCallback locationCallback;
     TextView textView;
+
+    final String user_id = "abc123";
 
 
     @Override
@@ -46,18 +62,20 @@ public class MainActivity extends AppCompatActivity {
         sitter_place.setLayoutManager(new LinearLayoutManager(this));
         list = new ArrayList<Sitter>();
 
-        requestPermission();
-
-
-
         fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this);
+        locationRequest = new LocationRequest();
 
+        locationRequest.setInterval(10000);
+        locationRequest.setFastestInterval(5000);
+        locationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+
+        requestPermission();
 
         reference = FirebaseDatabase.getInstance().getReference().child("Users").child("Cleaner");
         reference.addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
-                for (DataSnapshot dataSnapshot1: dataSnapshot.getChildren()){
+                for (DataSnapshot dataSnapshot1 : dataSnapshot.getChildren()) {
                     Sitter p = dataSnapshot1.getValue(Sitter.class);
                     list.add(p);
                 }
@@ -71,59 +89,76 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        textView.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                saveLocation();
-//                textView.setText("a");
-            }
-        });
-
-
     }
 
-    public void requestPermission(){
-        ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 1);
-    }
+    public void requestLoc(){
 
-    public void saveLocation(){
-        if (ActivityCompat.checkSelfPermission(MainActivity.this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+        if(ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) ==
+                PermissionChecker.PERMISSION_GRANTED && ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) ==
+                PermissionChecker.PERMISSION_GRANTED
+        ){
+            fusedLocationProviderClient.requestLocationUpdates(locationRequest, new LocationCallback() {
+                @Override
+                public void onLocationResult(LocationResult locationResult) {
+                    super.onLocationResult(locationResult);
+                    if(locationResult != null){
+                        final double latitude = locationResult.getLastLocation().getLatitude();
+                        final double longitude = locationResult.getLastLocation().getLongitude();
 
-            return;
+                        textView.setText(locationResult.toString());
+
+                        reference2 = FirebaseDatabase.getInstance().getReference().child("Users").child("Location").child(user_id);
+                        reference2.addListenerForSingleValueEvent(new ValueEventListener() {
+                            @Override
+                            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                                dataSnapshot.getRef().child("customer_lat").setValue(latitude);
+                                dataSnapshot.getRef().child("customer_lon").setValue(longitude);
+                            }
+
+                            @Override
+                            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+                            }
+                        });
+
+                    }
+                }
+            }, getMainLooper());
+        }
+        else {
+            requestPermission();
         }
 
-        fusedLocationProviderClient.getLastLocation().addOnSuccessListener(MainActivity.this, new OnSuccessListener<Location>() {
+    }
+
+
+    public void requestPermission(){
+        String[] permissions = {Manifest.permission.ACCESS_COARSE_LOCATION, Manifest.permission.ACCESS_FINE_LOCATION};
+        Permissions.check(this/*context*/, permissions, null/*rationale*/, null/*options*/, new PermissionHandler() {
             @Override
-            public void onSuccess(Location location) {
-                if(location != null){
-                    double latitude = location.getLatitude();
-                    double longitude = location.getLongitude();
-
-                    textView.setText(location.toString());
-
-//                    reference2 = FirebaseDatabase.getInstance().getReference().child("Users").child("Customer").child("vsdlk12");
-//                    GeoFire geoFire = new GeoFire(reference2);
-//                    geoFire.setLocation("location", new GeoLocation(latitude, longitude), new GeoFire.CompletionListener() {
-//
-//                        @Override
-//                        public void onComplete(String key, DatabaseError error) {
-//                            if(error != null){
-//                                Toast.makeText(getApplicationContext(), "Not saved", Toast.LENGTH_SHORT).show();
-//                            }
-//                            else {
-//                                Toast.makeText(getApplicationContext(), "saved", Toast.LENGTH_SHORT).show();
-//                            }
-//                        }
-//                    });
-
-                }
-            }
-        }).addOnFailureListener(new OnFailureListener() {
-            @Override
-            public void onFailure(@NonNull Exception e) {
-                Toast.makeText(getApplicationContext(), e.toString(), Toast.LENGTH_SHORT).show();
+            public void onGranted() {
+                // do your task.
+                requestLoc();
             }
         });
     }
 
+    @Override
+    protected void onStop() {
+        super.onStop();
+
+        reference3 = FirebaseDatabase.getInstance().getReference().child("Users").child("Location").child(user_id);
+        reference3.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                dataSnapshot.getRef().removeValue();
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+    }
 }
